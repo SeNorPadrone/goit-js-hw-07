09a8d8d38b253ac86e584e3699ed7ae4c8818b81
SeNorPadrone <90000708+SeNorPadrone@users.noreply.github.com>
2022-02-19 10:59:40 +0200

    1
    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const makeGalleryMarkup = (elements) => {
+  return elements.map(({ preview, original, description }) => {
+      return  `
+            
+            <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+            </a> `;
+}).join('');
+};
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', makeGalleryMarkup(galleryItems));
+
+
+
+lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,});
+
+    
+        
